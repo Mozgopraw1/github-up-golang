64390c043abe1f30a9a084e52fc2361a796492e2
Vladimir Balin <vladimir.balin@gmail.com>
2019-02-24 22:17:57 +0500

    базовый перебор репозиториев с временем обновления

--- go.mod
+++ go.mod
@@ -1,1 +1,3 @@
 module github.com/Mozgopraw1/github-up-golang
+
+require github.com/google/go-github/v24 v24.0.0
--- main.go
+++ main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"log"
+
+	"github.com/google/go-github/v24/github"
+)
 
 func main() {
-	fmt.Println("test")
+	client := github.NewClient(nil)
+	opt := &github.RepositoryListOptions{}
+	repos, _, err := client.Repositories.List(context.Background(), "Mozgopraw1", opt)
+
+	if err != nil {
+		log.Fatalf("failed to fetch repos: %s", err)
+	}
+
+	for _, repo := range repos {
+		log.Printf("%s\t%s\n", repo.GetUpdatedAt().Format("02 Jan 15:04:05"), repo.GetName())
+	}
 }
